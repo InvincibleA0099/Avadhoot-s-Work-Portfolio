66af62d2ed59642655b69bac83d7bcf420871f63
Avadhoot Gharat <avadhootgharat0099@gmail.com>
2024-08-18 19:10:27 +0530

    Offline to Online 1

---
+++ src/const/const.js
@@ -0,0 +1,81 @@
+export const studies = [
+  {
+    title: "Founder (Part-time)",
+    corporation: "Gharat Tours and Travels",
+    date: "03/2019 – Present (5 Years, 5 months)",
+  },
+  {
+    title: "Operations Analytics Intern",
+    corporation: "Economc Times B2B ",
+    date: "10/2023 – 04/2024 (6 months)",
+  },
+  {
+    title: "Asst. EV Dealership Manager",
+    corporation: "ShivShakti EV Motors, Navi Mumbai",
+    date: "12/2021 – 08/2022 (8 months)",
+  },
+];
+
+export const IconKeys = {
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  git: "git",
+  github: "github",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "",
+    title: "Sample Task - Invincible",
+    link: "",
+    description: `Kaam kr raha hain bhai`,
+    technologies: [
+      {
+        icon: IconKeys.typeScript,
+      },
+      {
+        icon: IconKeys.nextjs,
+      },
+      
+    ],
+  }
+];
